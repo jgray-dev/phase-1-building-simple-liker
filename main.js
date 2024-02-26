3868e766275132b8c44e610f82b714fed3d43ab8
Jackson Gray <nohaxjustdoge@gmail.com>
2024-02-26 21:22:01 +0000

    done

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
@@ -23,3 +20,29 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".like-glyph").forEach((button) => {
+    button.addEventListener("click", () => {
+      mimicServerCall()
+        .then((r) => {
+          if (button.classList.contains("activated-heart")) {
+            button.classList.remove("activated-heart");
+            button.textContent = EMPTY_HEART;
+          } else {
+            button.classList.add("activated-heart");
+            button.textContent = FULL_HEART;
+          }
+        })
+        .catch((error) => {
+          console.log("Catch error");
+          const errorDiv = document.getElementById("modal");
+          errorDiv.classList.remove("hidden");
+          errorDiv.textContent = error;
+          setTimeout(() => {
+            errorDiv.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
